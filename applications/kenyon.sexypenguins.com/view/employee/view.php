0412d6f5a4b31e6191d6e3b03e76fefe9eb0e2e1
Kenyon Haliwell <kenyon.jh@gmail.com>
2013-11-20 14:21:35 -0800

    Changed pay periods table to say Previous Pay Periods. Add login screen to screenshots

--- applications/kenyon.sexypenguins.com/view/employee/view.php
+++ applications/kenyon.sexypenguins.com/view/employee/view.php
@@ -61,7 +61,7 @@ $date_format = 'm/d/y';
                 <div class="col-sm-12">
                     <div class="panel panel-success">
                         <div class="panel-heading">
-                            <h3 class="panel-title">All Employees' Pay Periods</h3>
+                            <h3 class="panel-title">Previous Pay Periods</h3>
                         </div>
                         <div class="panel-body">
                             <div class="row">
