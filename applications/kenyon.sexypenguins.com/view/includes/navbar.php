bfec0f23036bc8145cf971c461635e223eb84d9f
Kenyon Haliwell <kenyon.jh@gmail.com>
2013-11-20 14:24:49 -0800

    Changed navbar to just say TimeClock. Added links to About page.

--- applications/kenyon.sexypenguins.com/view/includes/navbar.php
+++ applications/kenyon.sexypenguins.com/view/includes/navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a href="{timeclock_root}main" class="navbar-brand">TimeClock by Kenyon Haliwell</a>
+      <a href="{timeclock_root}main" class="navbar-brand">TimeClock</a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
